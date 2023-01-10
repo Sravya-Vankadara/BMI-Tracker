349b7650724d294153cf279e89917e15ea720070
Vishwakjith <66257576+Vishwakjith@users.noreply.github.com>
2023-01-10 09:32:45 +0530

    Front-End
    Yet to integrate with js.

---
+++ Front-End/js/navPage.js
@@ -0,0 +1,15 @@
+function mainPage() {
+    window.location.href = "mainPage.html";
+}
+
+function addPatient() {
+    window.location.href = "addPatient.html";
+}
+
+function searchPatient() {
+    window.location.href = "searchPatient.html";
+}
+
+function chartPatient() {
+    window.location.href = "chartPatient.html";
+}
---
+++ Front-End/js/searchPatient.js
@@ -0,0 +1,21 @@
+let patients = {
+    "ID" : 0,
+    "Name" : 1,
+    "Height" : 2,
+    "Weight" : 3
+}
+
+let patientsID = [];
+let patientsName = [];
+let patientsHeight = [];
+let patientsWeight = [];
+
+function viewPatients() {
+    let searchPatientName = document.getElementById("searchName").value
+    console.log(searchPatientName)
+    for( let i = 0 ; i < patientsName.length ; i++ ){
+        if ( patientsName == searchPatientName ){
+            
+        }
+    }
+}
